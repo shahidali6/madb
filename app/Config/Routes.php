2491cb8c8bccf1188eb0f271da44ca392a19aff9
Shahid Saddique <shahidali6@gmail.com>
2020-11-19 12:05:56 +0500

    change default function

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,7 +30,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Download::index');
 // $routes->get('users-list', 'UserCrud::index');
 // $routes->get('user-form', 'UserCrud::create');
 // $routes->post('submit-form', 'UserCrud::store');
